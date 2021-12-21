32f47fd60d95fdd0b44e669b8b16106e4927d879
Yogi Paturu <yogipaturu@Yogis-MacBook-Air.local>
2021-12-21 13:59:27 -0600

    cyclic sort

--- 05 - Cyclic Sort/01 - Cyclic Sort (easy).js
+++ 05 - Cyclic Sort/01 - Cyclic Sort (easy).js
@@ -1,7 +1,9 @@
 /*
-We are given an array containing n objects. Each object, when created, was assigned a unique number from the range 1 to n based on their creation sequence. This means that the object with sequence number 3 was created just before the object with sequence number 4.
+We are given an array containing n objects. Each object, when created, was assigned a unique number from the range 1 to n based on their creation sequence.
+This means that the object with sequence number 3 was created just before the object with sequence number 4.
 
-Write a function to sort the objects in-place on their creation sequence number in O(n)O(n) and without using any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+Write a function to sort the objects in-place on their creation sequence number in O(n) and without using any extra space.
+For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
 
 Example 1:
 Input: [3, 1, 5, 4, 2]
@@ -14,4 +16,41 @@ Output: [1, 2, 3, 4, 5, 6]
 Example 3:
 Input: [1, 5, 6, 4, 3, 2]
 Output: [1, 2, 3, 4, 5, 6]
+
+Validate argument
+Account for empty array
+
+Loop through array starting at 0
+    Swap with element at element - 1 index, until the current element is in the right one
+    increment
+Return array
+
+Time - O(N)
+Space - O(1)
  */
+
+
+const cyclicSort = (arr) => {
+    if(!Array.isArray(arr) || !arr.length) return [];
+
+    let i = 0,
+    j = 0;
+
+    while (i < arr.length){
+        j = arr[i] - 1;
+
+        if(arr[i] !== arr[arr[i] - 1]){ // element 1 must be in index 0, element 2 must be in index 1, so on..
+            [arr[i], arr[j]] = [arr[j], arr[i]] // swap with appropriate index (e.g., element 3 swap with index 2)
+        }
+        else i++;
+    }
+
+    return arr;
+}
+
+console.log(cyclicSort([1,2,3,4,5])); // [1,2,3,4,5]
+console.log(cyclicSort([4,5,1,3,2])); // [1,2,3,4,5]
+console.log(cyclicSort([1,4,2,3,5])); // [1,2,3,4,5]
+
+console.log(cyclicSort({})); // []
+console.log(cyclicSort([])); // []
