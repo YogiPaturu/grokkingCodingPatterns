52ee24ddbcecfdea5653bc45412838198261b6e9
Yogi Paturu <yogipaturu@Yogis-MacBook-Air.local>
2021-12-21 15:23:03 -0600

    reverse linkedlist

--- 06 - In-place Reversal of a LinkedList/01 - Reverse a LinkedList (easy).js
+++ 06 - In-place Reversal of a LinkedList/01 - Reverse a LinkedList (easy).js
@@ -1,3 +1,45 @@
 /*
 Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
+
+Time - O(N)
+Space - O(1)
 */
+
+class Node{
+    constructor(value, next = null){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const reverse = (head) => {
+    let prev = null,
+    curr = head,
+    next = curr.next;
+
+    while(curr !== null){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    
+    return prev;
+}
+
+
+let head = new Node(1);
+console.log(reverse(head)); // 1
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+console.log(reverse(head)); // 4 -> 3 -> 2 -> 1
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+console.log(reverse(head)); // 5 -> 4 -> 3 -> 2 -> 1
