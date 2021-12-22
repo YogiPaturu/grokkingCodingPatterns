f6b84e5a293de1787bc123c6ce9cf02f008e40b7
Yogi Paturu <yogipaturu@Yogis-MacBook-Air.local>
2021-12-22 11:56:26 -0600

    triplets with smaller sum

--- 02 - Two Pointers/06 - Triplets with Smaller Sum (medium).js
+++ 02 - Two Pointers/06 - Triplets with Smaller Sum (medium).js
@@ -13,6 +13,7 @@ Output: 4
 Explanation: There are four triplets whose sum is less than the target: [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
 
 Triplets with Smaller Sum:
+sort array
 initalize tripletsWithSmallerSum to 0
 loop thorugh array
     tripletsWithSmallerSum += invoke pair less than target function on the next index, associated target, and count
@@ -32,4 +33,49 @@ while they don't cross
         decrement right
 return count
 
+Time: O(NlogN + N*N) = O(NlogN + N^2) ≈ O(N^2)
+Space: O(N) for the sort
+
+Only using the test cases provided by the problem for the sake of time
 */
+
+const countPairsSmallerSum = (arr, startIndex, targetSum) => {
+    let pairs = 0,
+    left = startIndex,
+    right = arr.length - 1;
+
+    while(left < right){
+        if(arr[left] + arr[right] < targetSum){
+            pairs+=right-left;
+            left++;
+        }
+        else{
+            right--;
+        }
+    }
+
+    return pairs;
+}
+
+const countTripletsSmallerSum = (arr, target) => {
+    if(arr.length < 3) return 0;
+
+    let tripletsWithSmallerSum = 0,
+    pairStartIndex = 0,
+    pairTargetSum = 0;
+
+    arr.sort((a, b) => a - b);
+
+    for(let i = 0; i < arr.length; i++){
+        pairTargetSum = target - arr[i];
+        pairStartIndex = i + 1;
+        tripletsWithSmallerSum += countPairsSmallerSum(arr, pairStartIndex, pairTargetSum)
+    }
+
+    return tripletsWithSmallerSum;
+}
+
+
+
+console.log(countTripletsSmallerSum([-1, 0, 2, 3], 3)); // 2
+console.log(countTripletsSmallerSum([-1, 4, 2, 1, 3], 5)); // 4
