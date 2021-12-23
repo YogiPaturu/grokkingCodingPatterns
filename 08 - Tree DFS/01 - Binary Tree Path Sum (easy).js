461d217ac900cb79023a511a5ea814b940b15939
Yogi Paturu <yogipaturu@Yogis-MacBook-Air.local>
2021-12-23 17:06:03 -0600

    binary tree path sum

--- 08 - Tree DFS/01 - Binary Tree Path Sum (easy).js
+++ 08 - Tree DFS/01 - Binary Tree Path Sum (easy).js
@@ -1,3 +1,29 @@
 /*
 Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
+
+Time - O(N) such that N is the number of nodes
+Space - O(N) such that N is the number of nodes
 */
+
+class TreeNode{
+    constructor(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const hasSum = (root, sum) => {
+    if(root === null) return false;
+    if(root.val === sum && root.left === null && root.right === null) return true;
+    return hasSum(root.left, sum - root.val) || hasSum(root.right, sum - root.val)
+}
+
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.left.right = new TreeNode(5);
+console.log(hasSum(root, 23));
+console.log(hasSum(root, 16));
