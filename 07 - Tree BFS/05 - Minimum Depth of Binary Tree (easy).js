ccddb82f993efa5b2e6a1f9da453bf8c50063ab7
Yogi Paturu <yogipaturu@Yogis-MacBook-Air.local>
2021-12-23 10:53:42 -0600

    gitignore

---
+++ 07 - Tree BFS/05 - Minimum Depth of Binary Tree (easy).js
@@ -0,0 +1,5 @@
+/*
+Find the minimum depth of a binary tree. The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
+*/
+
+// npm install --save collections
