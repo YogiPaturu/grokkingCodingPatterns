f0a1548813eb40da6eb6e6394bee4cc52c94d74d
Yogi Paturu <ypaturu@gmail.com>
2022-01-12 10:21:18 -0600

    longest subarr with ones after replacement

--- 01 - Sliding Window/07 - Longest Subarray with Ones after Replacement (hard).js
+++ 01 - Sliding Window/07 - Longest Subarray with Ones after Replacement (hard).js
@@ -11,35 +11,47 @@ Example 2:
 Input: Array=[0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], k=3
 Output: 9
 Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguous subarray of 1s having length 9.
+
+sliding window problem
+what's the max window size, such that the window only has k number of 0's?
+decrement/increment k to keep track of number of 0's in the window
+increment windowEnd each iteration until k number of 0s in the window
+take the max of windowlength each time we have k number of 0s
+return max windowlength
+
+Time - O(N)
+Space - O(1)
 */
 
-/*sliding window, because finding continuous subarray within certain constraints
-loop through array
-    build dictionary with counts
-        increment value if it exists
-    determine the max length of dictionary between now and max
-    while, checking if (length of window - max length of dictionary) > k
-        decrement or remove dictionary value at window start
-        decrement window size
-    
-*/
 
 const longestSubArray = (array, k) => {
+    if(!Array.isArray(array) || array.length <= 0 || k < 0) return -1;
+    
+    let left = 0,
+    maxSize = 0,
+    ones = 0;
 
+    // expand the window size each iteration
+    for(let right = 0; right < array.length; right++){
+        // shrink when there are k number of 0's inside the window
+        if(array[right] === 1) ones++;
+        // this while loop ensures that the window size shrinks when there are too many (i.e., greater than k) zeros
+        
+        if((right - left + 1 - ones) > k){
+            if(array[left] === 1) ones--;
+            left++;
+        }
 
+        // capture maxSize AFTER ensuring window size is correct (i.e., not more than k number of zeros)
+        maxSize = Math.max(maxSize, right - left + 1);
+    }
+
+    return maxSize;
 }
 
-console.log(longestSubArray([0], 1)); // 1
-console.log(longestSubArray([0, 1], 1)); // 2
-console.log(longestSubArray([0, 0, 1, 1], 1)); // 3
-console.log(longestSubArray([0, 0, 1, 1, 1], 2)); // 5
-console.log(longestSubArray([1, 1, 1, 0, 0], 2)); // 5
-
-
-console.log(longestSubArray([1, 1, 1, 0, 0, 1, 1, 1], 2)); // 8
-console.log(longestSubArray([1, 1, 1, 0, 0, 1, 0, 1], 2)); // 6
-console.log(longestSubArray([1, 0, 1, 0, 0, 1, 1, 1], 2)); // 6
-
-console.log(longestSubArray([1, 1, 1, 0], -1)); // 0
-console.log(longestSubArray([], 2)); // 0
-console.log(longestSubArray({}, 2)); // 0
+console.log(longestSubArray([0,0,0,0], 2)); // 2
+console.log(longestSubArray([1,1,1,1], 2)); // 4
+console.log(longestSubArray([0,1,0,1], 2)); // 4
+console.log(longestSubArray([1,0,0,1], 2)); // 4
+console.log(longestSubArray([0,1,1,0], 2)); // 4
+console.log(longestSubArray([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2)); // 6
