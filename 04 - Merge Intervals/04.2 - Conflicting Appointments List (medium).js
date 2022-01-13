55ba9dcde99de428b66bd73499ea1ad91c33446e
Yogi Paturu <ypaturu@gmail.com>
2022-01-13 12:26:23 -0600

    conflicting list

---
+++ 04 - Merge Intervals/04.2 - Conflicting Appointments List (medium).js
@@ -0,0 +1,12 @@
+/*
+Problem 1: Given a list of appointments, find all the conflicting appointments.
+
+Example:
+
+Appointments: [[4,5], [2,3], [3,6], [5,7], [7,8]]
+Output: 
+[4,5] and [3,6] conflict. 
+[3,6] and [5,7] conflict.
+
+Same as the other one but instead of return true/false, push to an array to return later
+*/
