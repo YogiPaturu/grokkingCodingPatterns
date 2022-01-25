cf8c3741b34b1bdfd26ac22abb2f96527a1cf95d
Yogi Paturu <ypaturu@gmail.com>
2022-01-25 13:19:47 -0600

    more problems

---
+++ 07 - Tree BFS/07 - Connect Level Order Siblings (medium).js
@@ -0,0 +1,3 @@
+/*
+Given a binary tree, connect each node with its level order successor. The last node of each level should point to a null node.
+*/
---
+++ 15 - Knapsack DP/02 - Equal Subset Sum Partition (medium).js
@@ -0,0 +1,19 @@
+/*
+Given a set of positive numbers, find if we can partition it into two subsets such that the sum of elements in both subsets is equal.
+
+Example 1:
+
+Input: {1, 2, 3, 4}
+Output: True
+Explanation: The given set can be partitioned into two subsets with equal sum: {1, 4} & {2, 3}
+Example 2:
+
+Input: {1, 1, 3, 4, 7}
+Output: True
+Explanation: The given set can be partitioned into two subsets with equal sum: {1, 3, 4} & {1, 7}
+Example 3:
+
+Input: {2, 3, 4, 6}
+Output: False
+Explanation: The given set cannot be partitioned into two subsets with equal sum.
+*/
